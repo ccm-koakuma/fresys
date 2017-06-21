a8fba796044f6b09cb293537f59f7c24687d9bd1
Masakazu Ankyu <mskz625@gmail.com>
2017-06-21 22:23:41 +0900

    create part of update

--- edit.php
+++ edit.php
@@ -16,6 +16,28 @@ while($table = mysqli_fetch_assoc($record)){
   $datas_area[] = $table;
 }
 $count_area = count($datas_area);
+if(!empty($_POST)){
+  if($_POST['name'] == '' || $_POST['name'] == null){
+    $errors['name'] = 'blank';
+  }elseif($_POST['age'] == '' || $_POST['age'] == null){
+    $errors['age'] = 'blank';
+  }else{
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+    $areas_id = $_POST['area_id'];
+    $sql = sprintf('UPDATE `friends` SET `friend_name` = "%s", `area_id` = "%d", `gender` = "%s", `age` = "%d" WHERE `friends`.`friend_id` = "%s"',
+    mysqli_real_escape_string($db, $_POST['name']),
+    mysqli_real_escape_string($db, $_POST['area_id']),
+    mysqli_real_escape_string($db, $_POST['gender']),
+    mysqli_real_escape_string($db, $_POST['age']),
+    mysqli_real_escape_string($db, $_GET['id'])
+    );
+    mysqli_query($db, $sql) or die(mysqli_error($db));
+    header("Location:show.php?id=$areas_id");
+		exit();
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -36,6 +58,10 @@ $count_area = count($datas_area);
           <div class="content_title">名前</div>
           <div class="content_input">
             <input type="text" name="name" class="form-control" value="<?php echo $datas[0]['friend_name']; ?>">
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
+            <?php if (isset($errors['name']) && $errors['name'] == 'blank'): ?>
+              <p class="error" style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">*&nbsp;氏名を正しく入力してください</p>
+            <?php endif; ?>
           </div>
         </div>
         <div class="form-group">
@@ -50,21 +76,27 @@ $count_area = count($datas_area);
               endfor;
               ?>
             </select>
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
           </div>
         </div>
         <div class="form-group">
           <div class="content_title">性別</div>
           <div class="content_input">
             <select class="form-control" name="gender">
-              <option name="gender" value="男性" <?php if($datas[0]['gender'] == '男性') echo 'selected'; ?>>男性</option>
-              <option name="gender" value="女性" <?php if($datas[0]['gender'] == '女性') echo 'selected'; ?>>女性</option>
+              <option name="gender" value="m" <?php if($datas[0]['gender'] == 'm') echo 'selected'; ?>>男性</option>
+              <option name="gender" value="f" <?php if($datas[0]['gender'] == 'f') echo 'selected'; ?>>女性</option>
             </select>
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
           </div>
         </div>
         <div class="form-group">
           <div class="content_title">年齢</div>
           <div class="content_input">
             <input type="text" name="age" class="form-control" value="<?php echo $datas[0]['age']; ?>">
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
+            <?php if (isset($errors['age']) && $errors['age'] == 'blank'): ?>
+              <p class="error" style="color: red; font-size: 10px; margin: 0; margin-top: 2px;">*&nbsp;年齢を正しく入力してください</p>
+            <?php endif; ?>
           </div>
         </div>
         <input type="submit" class="btn btn-default" value="変更">
