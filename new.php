71d820831994f2808cae06093b950bc875a79d44
Masakazu Ankyu <mskz625@gmail.com>
2017-06-21 22:56:59 +0900

    create part of INSERT

--- new.php
+++ new.php
@@ -9,6 +9,25 @@ while($table = mysqli_fetch_assoc($record)){
   $datas_area[] = $table;
 }
 $count_area = count($datas_area);
+if(!empty($_POST)){
+  if($_POST['name'] == '' || $_POST['name'] == null){
+    $errors['name'] = 'blank';
+  }elseif($_POST['age'] == '' || $_POST['age'] == null){
+    $errors['age'] = 'blank';
+  }else{
+    $areas_id = $_POST['area_id'];
+    $sql = sprintf('INSERT INTO `friends` (`friend_id`, `friend_name`, `area_id`, `gender`, `age`, `created`, `modified`) VALUES (NULL, "%s", "%d", "%s", "%d", CURRENT_TIME(), CURRENT_TIMESTAMP)',
+    mysqli_real_escape_string($db, $_POST['name']),
+    mysqli_real_escape_string($db, $_POST['area_id']),
+    mysqli_real_escape_string($db, $_POST['age']),
+    mysqli_real_escape_string($db, $_POST['gender']),
+    mysqli_real_escape_string($db, $_GET['id'])
+    );
+    mysqli_query($db, $sql) or die(mysqli_error($db));
+    header("Location:show.php?id=$areas_id");
+		exit();
+  }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,6 +48,10 @@ $count_area = count($datas_area);
           <div class="content_title">名前</div>
           <div class="content_input">
             <input type="text" name="name" class="form-control" placeholder="例：安久　昌和">
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
+            <?php if (isset($errors['name']) && $errors['name'] == 'blank'): ?>
+              <p class="error" style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">*&nbsp;氏名を正しく入力してください</p>
+            <?php endif; ?>
           </div>
         </div>
         <div class="form-group">
@@ -44,6 +67,7 @@ $count_area = count($datas_area);
               endfor;
               ?>
             </select>
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
           </div>
         </div>
         <div class="form-group">
@@ -51,15 +75,20 @@ $count_area = count($datas_area);
           <div class="content_input">
             <select class="form-control" name="gender">
               <option value="0">性別を選択</option>
-              <option value="1">男性</option>
-              <option value="2">女性</option>
+              <option name="gender" value="m">男性</option>
+              <option name="gender" value="f">女性</option>
             </select>
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
           </div>
         </div>
         <div class="form-group">
           <div class="content_title">年齢</div>
           <div class="content_input">
             <input type="text" name="age" class="form-control" placeholder="例: 22">
+            <span style="color: red; font-size: 10px; margin: 0; margin-top: 10px;">&nbsp;&nbsp;*&nbsp;必須</span>
+            <?php if (isset($errors['age']) && $errors['age'] == 'blank'): ?>
+              <p class="error" style="color: red; font-size: 10px; margin: 0; margin-top: 2px;">*&nbsp;年齢を正しく入力してください</p>
+            <?php endif; ?>
           </div>
         </div>
         <input type="submit" class="btn btn-default" value="登録">
