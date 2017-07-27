7d38283e67aa47886201894759b032993e1da25c
matsuoka shiho <shihopine10-marie11@docomo.ne.jp>
2017-07-27 19:41:17 +0900

    show追加

--- show.php
+++ show.php
@@ -1,70 +1,26 @@
 <?php
-include('dbconnect.php');
+require('dbconnect.php');
 if(!empty($_GET['id']) && isset($_GET['id'])){
-  $sql = sprintf('SELECT * FROM `friends`, `areas` WHERE areas.area_id = friends.area_id AND friends.area_id=%s',$_GET['id']);
-  $record = mysqli_query($db,$sql) or die(mysqli_error($db));
-  while($table = mysqli_fetch_assoc($record)){
+$sql=sprintf('SELECT * FROM `friends`,`areas` WHERE friends.area_id=areas.area_id AND friends.area_id=%s', $_GET['id']);
+$record=mysqli_query($db,$sql) or die(mysqli_error($db));
+while($table=mysqli_fetch_assoc($record)){
     $datas[] = $table;
-  }
+    }
+    $count=count($datas);
+echo "<pre>";
+print_r($datas);
+echo "</pre>";
 }else{
-  header("index.php");
+  header('LOcation: index.php');
   exit();
-}
-$count = count($datas);
-$data_male = 0;
-$data_female = 0;
-for($i = 0; $i < $count; $i++){
-  if(isset($datas[$i]['gender']) && $datas[$i]['gender'] == 'm'){
-    $data_male += 1;
-  }elseif(isset($datas[$i]['gender']) && $datas[$i]['gender'] == 'f'){
-    $data_female += 1;
   }
-}
+  $datas_male = 0;
+  $datas_female = 0;
+  for($i=0; $i<$count; $i++){
+   if(isset($datas[$i]['gender']) && $datas[$i]['gender'] == 'm'){
+    $datas_male += 1;
+   }elseif(isset($datas[$i]['gender']) && $datas[$i]['gender'] == 'f'){
+    $datas_female += 1;
+   }
+  }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>都道府県お友達システム</title>
-    <link rel="stylesheet" href="./assets/css/bootstrap.css">
-    <link rel="stylesheet" href="./assets/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-  </head>
-  <body>
-    <div class="contents">
-      <div class="contents_title">
-        <h1><?php echo $datas[0]['area_name']; ?>友達一覧</h1>
-      </div>
-      <div class="well">
-        男性:<?php echo $data_male; ?>名&nbsp;女性:<?php echo $data_female; ?>名
-      </div>
-      <table class="table table-striped table-hover table-condensed">
-        <thead>
-          <tr>
-            <th><div class="text-center">名前</div></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          for($i = 0; $i < $count; $i++):
-          ?>
-            <tr>
-              <td><?php echo htmlspecialchars($datas[$i]['friend_name']); ?></td>
-              <td>
-                <a href="edit.php?id=<?php echo $datas[$i]['friend_id']; ?>"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
-                <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
-              </td>
-            </tr>
-          <?php
-          endfor;
-          ?>
-        </tbody>
-      </table>
-      <input type="button" class="btn btn-default" value="新規作成" onClick="location.href='new.php'">
-    </div>
-    <div class="copyright">
-      <small>Copyright &copy; Core Creative Manager.All right reserved.</small>
-    </div>
-  </body>
-</html>
